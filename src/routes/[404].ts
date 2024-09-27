76de04bef5774a6565903686b47b859417c2421e
realkhoa <khoa.nguyenba@outlook.com>
2024-09-27 17:55:08 +0700

    add 404 handler

---
+++ src/routes/[404].ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import ResponseBuilder from "../services/responseBuilder";
+
+const notfound = Router();
+
+notfound.all("/", (req, res) => {
+  return ResponseBuilder.NotFound(res, "REQUESTED_CONTENT_NOTFOUND");
+});
+
+export default notfound;
