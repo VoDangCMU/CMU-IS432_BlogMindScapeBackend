d159a808ec4b77bf74327d1b41570c34cbf96c22
realkhoa <khoa.nguyenba@outlook.com>
2024-10-28 13:44:01 +0700

    add error catcher to purge database

--- src/messageCodes.ts
+++ src/messageCodes.ts
@@ -4,7 +4,8 @@ const MessageCodes = {
     USER_NOT_EXISTED:"Cannot found user",
     POST_NOT_EXISTED: "Cannot found post",
     NOT_OWN_POST: "You cannot do this interaction to others' post",
-    NaN: " is not a number"
+    NaN: " is not a number",
+    DATABASE_BUSY: "Database is being accessed by other users"
 }
 
 export default MessageCodes;
--- src/routes/test_only/db/purgeDB.ts
+++ src/routes/test_only/db/purgeDB.ts
@@ -4,15 +4,23 @@ import env from "@root/env";
 import ResponseBuilder from "@services/responseBuilder";
 import {createDatabase, dropDatabase} from "typeorm-extension";
 import log from "@services/logger";
+import e from "cors";
+import MessageCodes from "@root/messageCodes";
 
 export default async function purgeDB(req: Request, res: Response) {
-	if (env.ENV == 'production')
+	if (env.ENV == 'production') {
 		return ResponseBuilder.Forbidden(res);
+	}
 
-	// log.info(AppDataSourceOptions)
+	try {
+		await dropDatabase({ifExist: true, options: AppDataSourceOptions, initialDatabase: "postgres"});
+		await createDatabase({ifNotExist: true, options: AppDataSourceOptions, initialDatabase: "postgres"});
+	} catch (err) {
+		log.error(err);
+		return ResponseBuilder.InternalServerError(res, MessageCodes.DATABASE_BUSY);
+	}
 
-	await dropDatabase({ifExist: true, options: AppDataSourceOptions, initialDatabase: "postgres"});
-	await createDatabase({ifNotExist: true, options: AppDataSourceOptions, initialDatabase: "postgres"});
+
 
 	ResponseBuilder.Ok(res);
 }
